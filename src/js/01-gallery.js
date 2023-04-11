69276e516f158b2ef233c1d8ab829020681ec41e
Jura Mazar <ura2003mazar@gmail.com>
2023-04-11 11:12:16 +0300

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+(function () {
+    const marcup = galleryItems.map(({preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" target="_self" href="${original}">
+    <img
+      class="gallery__image js-target"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+    gallery.insertAdjacentHTML('beforeend', marcup)
+})();
+
+
+
+
+let galleryLightBox = new SimpleLightbox('.gallery a', {
+   
+    captionData: 'alt',
+    captionDelay: 250,
+});
