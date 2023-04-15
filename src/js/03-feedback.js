ecbf5c4f6121260898972f222941283ba0ea6282
Jura Mazar <ura2003mazar@gmail.com>
2023-04-15 23:21:09 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -25,8 +25,8 @@ function onSubmitForm(evt) {
   localStorage.removeItem('feedback-form-state');
   window.localStorage.clear();
   formData = {
-  email: actualObj.email ?? "",
-  message: actualObj.message ?? ""
+  email: "",
+  message: ""
   };
  
 };
